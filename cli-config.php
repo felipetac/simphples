6082559e8f421b6d3895745d1d474275262eb4fa
Felipe Toscano de Azevedo Cardoso <felipe.toscano@gmail.com>
2013-01-17 20:18:17 -0200

    Exclusão de alguns códigos comentados

--- cli-config.php
+++ cli-config.php
@@ -1,9 +1,6 @@
 <?php
 
-//define('SITE_URL','http://localhost/escola2/');
 define('SITE_PATH',realpath(dirname("application")).'/');
-//define('CONTROLLER_PATH', SITE_PATH . 'application/controllers');
-//define('VIEW_PATH', SITE_PATH . 'application/views');
 define('MODEL_PATH', SITE_PATH . 'application/models');
 
 require 'vendor/autoload.php';
