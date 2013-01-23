f21202fcb4afbc3dc49981ca3e9252a517bb7cc5
Ricardo Amorim <ricardoasilva2@gmail.com>
2013-01-23 15:44:44 -0200

    Adicionado a pasta Respect/Validate

---
+++ Respect/Validation/Exceptions/MostOfException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Respect\Validation\Exceptions;
+
+class MostOfException extends AtLeastException
+{
+
+}
---
+++ Respect/Validation/Rules/Json.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Respect\Validation\Rules;
+
+class Json extends AbstractRule
+{
+
+    public function validate($input)
+    {
+        return (bool) (json_decode($input));
+    }
+
+}
